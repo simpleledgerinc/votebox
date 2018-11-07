0a7ca7a6bb02f98ae2288601f3949383a08d8fca
Simon Hilchenbach <simon@hilchenba.ch>
2018-11-07 22:20:10 +0100

    Fix badger message

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import {
-  Switch,
-  Route
+    Switch,
+    Route
 } from "react-router-dom";
 import {
-  Container,
-  Divider,
-  Header,
-  Menu,
-  Segment
+    Container,
+    Divider,
+    Header,
+    Menu,
+    Segment
 } from 'semantic-ui-react';
 import BadgerMessage from './BadgerMessage';
 import CreateBallot from './CreateBallot';
@@ -21,42 +21,42 @@ import './App.css';
 import logo from '../images/votebox-logo.png';
 
 class App extends Component {
-  render() {
-    return (
-      <Container className="App">
-        <Segment>
-          <div className='app-header'>
-            <Header as='h2' icon>
-              <img className="logo" src={logo} /> <br />
-              <Header.Subheader>The Decentralized Voting and Election System for Bitcoin.</Header.Subheader>
-            </Header>
-            <BadgerMessage />
-          </div>
-          <Divider />
-          <Menu pointing secondary>
-            <MenuItem exact to='/'>
-              Create New Vote Token
-            </MenuItem>
-            <MenuItem to='/votes'>
-              Cast My Votes
-            </MenuItem>
-            <MenuItem to='/ballots'>
-              Find Voting Results
-            </MenuItem>
-            <MenuItem exact to='/airdrop'>
-              Distribute Vote Tokens
-            </MenuItem>
-          </Menu>
-          <Switch>
-            <Route exact path='/' component={CreateBallot} />
-            <Route path='/ballots' component={BallotFinder} />
-            <Route path='/votes' component={MyVotes} />
-            <Route path='/airdrop' component={AirdropCreate} />
-          </Switch>
-        </Segment>
-      </Container>
-    );
-  }
+    render() {
+        return (
+            <Container className="App">
+                <Segment>
+                    <div className='app-header'>
+                        <Header as='h2' icon>
+                            <img className="logo" src={logo} /> <br />
+                            <Header.Subheader>The Decentralized Voting and Election System for Bitcoin.</Header.Subheader>
+                        </Header>
+                        <BadgerMessage />
+                    </div>
+                    <Divider />
+                    <Menu pointing secondary>
+                        <MenuItem exact to='/'>
+                            Create New Vote Token
+                        </MenuItem>
+                        <MenuItem to='/votes'>
+                            Cast My Votes
+                        </MenuItem>
+                        <MenuItem to='/ballots'>
+                            Find Voting Results
+                        </MenuItem>
+                        <MenuItem exact to='/airdrop'>
+                            Distribute Vote Tokens
+                        </MenuItem>
+                    </Menu>
+                    <Switch>
+                        <Route exact path='/' component={CreateBallot} />
+                        <Route path='/ballots' component={BallotFinder} />
+                        <Route path='/votes' component={MyVotes} />
+                        <Route path='/airdrop' component={AirdropCreate} />
+                    </Switch>
+                </Segment>
+            </Container>
+        );
+    }
 }
 
 export default App;
--- src/containers/BadgerMessage.js
+++ src/containers/BadgerMessage.js
@@ -2,12 +2,35 @@ import React, { PureComponent } from 'react';
 import {
     Message
 } from 'semantic-ui-react';
-
 import BadgerWallet from '../lib/BadgerWallet';
 
 export default class BadgerMessage extends PureComponent {
+    state = {
+        installed: BadgerWallet.hasInstalled(),
+        loggedIn: BadgerWallet.isLoggedIn()
+    };
+
+    componentDidMount(){
+        this._intervalId = setInterval(this.checkBadger, 1000);
+    }
+
+    componentWillUnmount(){
+        clearInterval(this._intervalId);
+    }
+
+    checkBadger = () => {
+        const update = {};
+        if(BadgerWallet.hasInstalled() !== this.state.installed){
+            update.installed = BadgerWallet.hasInstalled();
+        }
+        if(BadgerWallet.isLoggedIn() !== this.state.loggedIn){
+            update.loggedIn = BadgerWallet.isLoggedIn()
+        }
+        this.setState(update);
+    }
+
     render() {
-        if (!BadgerWallet.hasInstalled()) {
+        if (!this.state.installed) {
             return (
                 <Message info>
                     <Message.Header>Optional: Install BadgerWallet for optimized voting experience</Message.Header>
@@ -16,7 +39,7 @@ export default class BadgerMessage extends PureComponent {
             );
         }
 
-        if(!BadgerWallet.isLoggedIn()){
+        if(!this.state.loggedIn){
             return (
                 <Message warning>
                     <Message.Header>You are not logged in to BadgerWallet</Message.Header>
